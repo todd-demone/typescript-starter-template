c4133a75f7920bbf7b49034a451a15bc4d8874d3
Todd Demone <tdemone1@gmail.com>
2025-12-11 15:56:41 -0500

    add eslint-plugin-prettier and eslint-config-prettier so eslint format rules that conflict with prettier are not applied

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,28 @@
 // @ts-check
 
-import eslint from "@eslint/js";
 import { defineConfig } from "eslint/config";
+import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import globals from "globals";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   {
+    files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
+      parser: tseslint.parser,
       globals: {
         ...globals.browser,
       },
+      parserOptions: {
+        project: true,
+      },
+    },
+    rules: {
+      quotes: ["error", "double"],
     },
   },
+  eslintPluginPrettierRecommended,
 );
